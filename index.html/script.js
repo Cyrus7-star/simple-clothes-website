b302a90c5d984a24bc9b897f0d037c756dc29d88
Cyrus7-star <brothercyrusshepherd@gmail.com>
2025-06-20 00:58:57 +0530

    Add files via upload

--- index.html/script.js
+++ index.html/script.js
@@ -1,4 +1,14 @@
 function addToCart(item) {
   document.getElementById('cart').innerHTML = `<p>${item} added to cart!</p>`;
 }
+function filterProducts() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const products = document.getElementsByClassName("product");
+
+  for (let i = 0; i < products.length; i++) {
+    const name = products[i].getElementsByTagName("h2")[0].innerText.toLowerCase();
+    products[i].style.display = name.includes(input) ? "block" : "none";
+  }
+}
+
 
